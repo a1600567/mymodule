2e23bb37d536c3f05a9847a0afb8757c8a7761b8
a1600567 <a1600567@myy.haaga-helia.fi>
2017-05-16 12:42:48 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install -y puppet
 sudo cp -r ./javamodule /etc/puppet/modules/asennusmoduuli
 sudo puppet apply -e 'class {asennusmoduuli:}'
+firefox 'localhost'
 
  
 
